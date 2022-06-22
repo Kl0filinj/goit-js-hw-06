90375ddd96dd5e2e7a9262ac3a9ba0f775334535
Kl0filinj <vladdanya08@gmail.com>
2022-06-22 18:54:32 +0300

    task 8 - clt

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector('.login-form');
+// const
 
+const submitingForm = event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Заполните пожалуйста все поля !');
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+formRef.addEventListener('submit', submitingForm);
