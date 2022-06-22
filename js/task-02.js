01b45fcfcd0975f80b8c3e64d04f9b00f422c787
Kl0filinj <vladdanya08@gmail.com>
2022-06-22 13:59:11 +0300

    task 2 - clt

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListRef = document.querySelector('#ingredients');
+
+const newIngredientsList = ingredients.map(item => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.textContent = item;
+  ingredientEl.classList.add('item');
+  return ingredientEl;
+});
+
+ingredientsListRef.append(...newIngredientsList);
