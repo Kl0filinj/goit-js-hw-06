a98482c0750a19943e7ca33c212112a95e8c5d59
Kl0filinj <vladdanya08@gmail.com>
2022-06-22 19:04:47 +0300

    task 9 - clt

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorNameRef = document.querySelector('.color');
+const btnChangeColorRef = document.querySelector('.change-color');
+
+const changeBgColor = () => {
+  colorNameRef.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
+};
+
+btnChangeColorRef.addEventListener('click', changeBgColor);
