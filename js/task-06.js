9d9e56b2b1a5178d1a19e88e451069968fc0ea19
Kl0filinj <vladdanya08@gmail.com>
2022-06-26 12:42:47 +0300

    df

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,12 @@
 const inputRef = document.querySelector('#validation-input');
 
 const validationForInput = event => {
-  if (event.currentTarget.value.length > inputRef.dataset.length) {
-    inputRef.classList.add('invalid');
-  } else {
+  if (event.currentTarget.value.length == event.currentTarget.dataset.length) {
     inputRef.classList.add('valid');
+    inputRef.classList.remove('invalid');
+  } else {
+    inputRef.classList.add('invalid');
+    inputRef.classList.remove('valid');
   }
 };
 
--- js/task-08.js
+++ js/task-08.js
@@ -10,10 +10,12 @@ const submitingForm = event => {
 
   if (email.value === '' || password.value === '') {
     return alert('Заполните пожалуйста все поля !');
+  } else {
+    console.log(
+      `${email.name}: ${email.value}, ${password.name}: ${password.value}`,
+    );
+    event.currentTarget.reset();
   }
-
-  console.log(`Email: ${email.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
 };
 
 formRef.addEventListener('submit', submitingForm);
